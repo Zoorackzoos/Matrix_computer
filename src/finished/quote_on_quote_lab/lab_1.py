efd55503c23ab061d7080c98a199a2a33eaede5c
Duncan Holmes <duncancholmes@gmail.com>
2026-03-13 10:51:18 -0500

    form whom the lab trolls

---
+++ src/finished/quote_on_quote_lab/lab_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+A = np.array \
+  (
+      [
+        [1, 2, 3, 4],
+        [4, 5, 6, 7],
+        [7, 8, 9, 10]
+      ]
+  )
+
+# task 2. fuck you i know how to do matrices in python.
+# fuck you i'im not doing this
