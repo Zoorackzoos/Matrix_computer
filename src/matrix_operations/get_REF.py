deddfd584d1d06e1858eaab262df86c4fa1cca5d
Duncan Holmes <duncancholmes@gmail.com>
2026-03-25 10:13:01 -0500

    get_REF: try 4.

--- src/matrix_operations/get_REF.py
+++ src/matrix_operations/get_REF.py
@@ -9,11 +9,12 @@ from src.matrix_operations.vector_multiplier import *
 def get_if_matrix_is_all_zeros(matrix_in_question, tab_amount="\t"):
     print(tab_amount,"check_if_matrix_is_all_zeros")
     tab_amount += "\t"
+    print_matrix(matrix=matrix_in_question,tab_amount=tab_amount)
     for row in matrix_in_question:
         for column in row:
-            if isinstance(matrix_in_question[row][column], numbers.Number):
-                return True
-    return False
+            if isinstance(column, numbers.Number):
+                return False
+    return True
 
 def get_REF(matrix_in_question,tab_amount="\t"):
     """
@@ -43,11 +44,11 @@ def get_REF(matrix_in_question,tab_amount="\t"):
     tab_amount += "\t"
 
     if len(matrix_in_question) == 0:
-        print("empty list. wtf dude.")
+        print(tab_amount,"empty list. wtf dude.")
         exit(2)
 
     if get_if_matrix_is_all_zeros(matrix_in_question=matrix_in_question,tab_amount=tab_amount):
-        print("this list is all zeros. wtf dude.")
+        print(tab_amount,"this list is all zeros. wtf dude.")
         exit(2)
 
     num_of_rows = len(matrix_in_question)
@@ -62,12 +63,60 @@ def get_REF(matrix_in_question,tab_amount="\t"):
 
     row_index_a = 0
     row_index_b = 0
+    column_index_a = 0
+    column_index_b = 0
+    column_index_shared = 0
+
+    print(tab_amount,"loop time :DDD")
+    tab_amount += "\t"
 
     while row_index_a < num_of_rows:
+        print(tab_amount,"row_index_a = ",row_index_a)
         while row_index_b < num_of_columns:
+            print(tab_amount+"\t","row_index_b = ",row_index_b)
 
+            # if we're comparing the same rule. do nothing
+            if matrix_in_question[row_index_a] == matrix_in_question[row_index_b]:
+                print(tab_amount+"\t\t","we're comparing the same row. So no operation here.")
+                row_index_b += 1
+            else:
+                old_matrix_in_question = copy.deepcopy(matrix_in_question)
 
-    exit(999)
+                #multiplying the rows
+                print(tab_amount+"\t\t","multiplying the rows so we can get a pivot when we find the sum of both of them easier.")
+                scale_row_from_number(matrix=matrix_in_question,row_in_question=row_index_a,number=old_matrix_in_question[row_index_b][column_index_shared],tab_amount=tab_amount+"\t\t\t")
+                scale_row_from_number(matrix=matrix_in_question,row_in_question=row_index_b,number=old_matrix_in_question[row_index_a][column_index_shared],tab_amount=tab_amount+"\t\t\t")
+
+                #dealing with both positive and both negative rows.
+                print(tab_amount+"\t\t",f"is row {row_index_a} and row {row_index_b} both positive or both negative?")
+                both_rows_positive = (matrix_in_question[row_index_a][column_index_shared] > 0
+                                        and
+                                        matrix_in_question[row_index_b][column_index_shared] > 0)
+                both_rows_negative = (matrix_in_question[row_index_a][column_index_shared] < 0
+                                        and
+                                        matrix_in_question[row_index_b][column_index_shared] < 0)
+
+                if both_rows_positive or both_rows_negative:
+                    print(tab_amount+"\t\t\t",f"They were either both positive or negative. So i set row {row_index_b} * -1")
+                    scale_row_from_number(matrix=matrix_in_question,row_in_question=row_index_b,number=-1,tab_amount=tab_amount+"\t\t\t")
+                else:
+                    print(tab_amount+"\t\t\t","they were opposite signs so no operation needed.")
+
+                #finding the sum of the 2 rows
+                print(tab_amount+"\t\t","finding the sum of the 2 rows.")
+                scale_row_from_row_and_number(matrix=matrix_in_question,row_modified=row_index_b,row_to_be_added=row_index_a,number=1,tab_amount=tab_amount+"\t\t\t")
+
+                #compare and contrast matrices.
+                print(tab_amount+"\t\t","matrix_in_question")
+                print_matrix(matrix=matrix_in_question,tab_amount=tab_amount+"\t\t\t")
+                print(tab_amount+"\t\t","old_matrix_in_question")
+                print_matrix(matrix=old_matrix_in_question,tab_amount=tab_amount+"\t\t\t")
+
+                if row_index_b == 2:
+                    exit(999)
+
+                row_index_b += 1
+
 
 if __name__ == "__main__":
     print("start of program")
