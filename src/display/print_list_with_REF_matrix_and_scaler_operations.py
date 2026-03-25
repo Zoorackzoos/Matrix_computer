42e455c80ee4d0c6aeb9abd70c5d639940ee0948
Duncan Holmes <duncancholmes@gmail.com>
2026-03-25 15:19:31 -0500

    dammit

---
+++ src/display/print_list_with_REF_matrix_and_scaler_operations.py
@@ -0,0 +1,7 @@
+from src.display.print_matrix import *
+
+def print_list_with_REF_matrix_and_scaler_operations(list_with_REF_matrix_and_scaler_operations, tab_amount="\t"):
+    print(tab_amount,"print_list_with_REF_matrix_and_scaler_operations")
+    tab_amount += "\t"
+    print_matrix(matrix=list_with_REF_matrix_and_scaler_operations[0],tab_amount=tab_amount)
+    print(tab_amount,list_with_REF_matrix_and_scaler_operations[1])
--- src/questions/5/5_2/q_5_2_4.py
+++ src/questions/5/5_2/q_5_2_4.py
@@ -5,6 +5,7 @@ from src.matrix_operations.get_REF import *
 from src.matrix_operations.operation_functions import *
 from src.matrix_operations.set_matrix_pivots_into_ones import *
 from src.matrix_operations.vector_multiplier import *
+from src.display.print_list_with_REF_matrix_and_scaler_operations import *
 
 def q_5_2_4_a(tab_amount="\t"):
     print(tab_amount,"q_5_2_4_a")
@@ -18,10 +19,15 @@ def q_5_2_4_a(tab_amount="\t"):
         [-5,-1,9,4]
     ]
     matrix_a_REF_and_scaler_ops_list = get_list_with_REF_and_return_determinant_values(matrix_in_question=matrix_a,tab_amount=tab_amount)
-    determinant = get_determinant_based_on_list_with_REF_matrix_and_scaler_operations_in_it(list_with_REF_matrix_and_scaler_operations_in_it=matrix_a_REF_and_scaler_ops_list,tab_amount=tab_amount)
-    print(tab_amount,"determinant = ",determinant)
+    print_list_with_REF_matrix_and_scaler_operations(list_with_REF_matrix_and_scaler_operations=matrix_a_REF_and_scaler_ops_list,tab_amount=tab_amount)
+
+    matrix_a_determinant = get_determinant_based_on_list_with_REF_matrix_and_scaler_operations_in_it(list_with_REF_matrix_and_scaler_operations_in_it=matrix_a_REF_and_scaler_ops_list,tab_amount=tab_amount)
+    print(tab_amount,"matrix_a_determinant = ",matrix_a_determinant)
 
 def q_5_2_4_b(tab_amount="\t"):
+    print(tab_amount,"q_5_2_4_b")
+    tab_amount += "\t"
+
     matrix_b = \
     [
         [-3,9,18,36],
@@ -29,7 +35,16 @@ def q_5_2_4_b(tab_amount="\t"):
         [-1,1,-2,-7],
         [-1,3,6,12]
     ]
+    matrix_b_REF_and_scaler_ops_list = get_list_with_REF_and_return_determinant_values(matrix_in_question=matrix_b,tab_amount=tab_amount)
+    print_list_with_REF_matrix_and_scaler_operations(list_with_REF_matrix_and_scaler_operations=matrix_b_REF_and_scaler_ops_list,tab_amount=tab_amount)
 
+    determinant_of_matrix_b = get_determinant_based_on_list_with_REF_matrix_and_scaler_operations_in_it(list_with_REF_matrix_and_scaler_operations_in_it=matrix_b_REF_and_scaler_ops_list,tab_amount=tab_amount)
+    print(tab_amount,"determinant_of_matrix_b = ",determinant_of_matrix_b)
 
 if __name__ == "__main__":
-    q_5_2_4_a(tab_amount="")
+    print("program started")
+    tab_amount = "\t"
+
+    q_5_2_4_b(tab_amount=tab_amount)
+
+    print("program ended")
