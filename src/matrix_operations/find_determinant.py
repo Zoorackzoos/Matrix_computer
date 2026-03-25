8e9d96f8025b18843e20104c338a8efe686e5022
Duncan Holmes <duncancholmes@gmail.com>
2026-03-25 14:51:22 -0500

    tested. might be in spagetti code hell. hope not though :-/

--- src/matrix_operations/find_determinant.py
+++ src/matrix_operations/find_determinant.py
@@ -1,3 +1,5 @@
+from src.matrix_operations.get_REF import *
+
 def get_determinant_two_by_two_matrix(matrix, tab_amount="\t"):
     """
 
@@ -31,5 +33,39 @@ def get_diagonal_values_from_matrix(matrix_in_question,tab_amount="\t"):
         diagonal_values_list.append(matrix_in_question[diagonal_index][diagonal_index])
     return diagonal_values_list
 
+def get_determinant_based_on_list_with_REF_matrix_and_scaler_operations_in_it(list_with_REF_matrix_and_scaler_operations_in_it,tab_amount="\t"):
+    print(tab_amount,"get_determinant_based_on_list_with_REF_matrix_and_scaler_operations_in_it")
+    tab_amount += "\t"
+
+    #making product of diagonal values list
+    diagonal_values_list = get_diagonal_values_from_matrix(matrix_in_question=list_with_REF_matrix_and_scaler_operations_in_it[0],tab_amount=tab_amount)
+
+    product_of_diagonal_values_list = diagonal_values_list[0]
+    diagonal_values_list_index = 1
+
+    while diagonal_values_list_index < len(diagonal_values_list):
+        product_of_diagonal_values_list *= diagonal_values_list[diagonal_values_list_index]
+        diagonal_values_list_index += 1
+
+    return product_of_diagonal_values_list
+
 if __name__ == "__main__":
-    print("hello me, meet the real me.")
+    print("start of program")
+    matrix_in_question = \
+        [
+            [-5, 1, 11, 3],
+            [-4, -5, 3, 3],
+            [2, 3, -1, 4],
+            [-5, -1, 9, 4]
+        ]
+
+    tab_amount = "\t"
+
+    REF_of_matrix_in_question_and_its_determinant_values = get_REF_and_return_determinant_values(matrix_in_question=matrix_in_question,tab_amount=tab_amount)
+
+    print_matrix(REF_of_matrix_in_question_and_its_determinant_values[0],tab_amount=tab_amount)
+    print(tab_amount,REF_of_matrix_in_question_and_its_determinant_values[1])
+
+    determinant = get_determinant_based_on_list_with_REF_matrix_and_scaler_operations_in_it(list_with_REF_matrix_and_scaler_operations_in_it=REF_of_matrix_in_question_and_its_determinant_values,tab_amount=tab_amount)
+    print(tab_amount,determinant)
+    print("end of program")
