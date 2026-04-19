762c2feadccd3715db4939a1252af438f8c4cb36
Zoorackzoos <duncancholmes@gmail.com>
2026-04-18 21:09:24 -0500

    confusion

---
+++ src/A_universal_operations/calc_3_like_funcitons/get_if_three_matrices_are_perpandicular.py
@@ -0,0 +1,54 @@
+from src.A_universal_operations.calc_3_like_funcitons.get_dot_product import get_dot_product
+
+
+def get_if_three_matrices_are_perpendicular(matrix_a, matrix_b, matrix_c, tab_amount ="\t"):
+    """
+    To determine if three vectors are perpendicular to each other (mutually perpendicular),
+     you must verify that the dot product of every possible pair is zero. For three vectors
+
+    :param matrix_a:
+    :param matrix_b:
+    :param matrix_c:
+    :param tab_amount:
+    :return:
+    """
+    print(tab_amount,"get_if_three_matrices_are_perpendicular")
+    tab_amount += "\t"
+
+    ab = get_dot_product(matrix_a=matrix_a, matrix_b=matrix_b,tab_amount=tab_amount)
+    ac = get_dot_product(matrix_a=matrix_a, matrix_b=matrix_c,tab_amount=tab_amount)
+    bc = get_dot_product(matrix_a=matrix_b, matrix_b=matrix_c,tab_amount=tab_amount)
+
+    return_bool = False
+
+    ab_equals_zero_bool = ab == 0
+    print(tab_amount,"ab_equals_zero_bool = ",ab_equals_zero_bool)
+    ac_equals_zero_bool = ac == 0
+    print(tab_amount,"ac_equals_zero_bool = ",ac_equals_zero_bool)
+    bc_equals_zero_bool = bc == 0
+    print(tab_amount,"bc_equals_zero_bool = ",bc_equals_zero_bool)
+
+    if ab_equals_zero_bool and ac_equals_zero_bool and bc_equals_zero_bool:
+        return_bool = True
+
+    print(tab_amount,"return_bool = ",return_bool)
+    return return_bool
+
+
+if __name__ == "__main__":
+    tab_amount = "\t"
+
+    matrix_a = \
+        [
+            [1, 2, 3]
+        ]
+    matrix_b = \
+        [
+            [1, -1, 2]
+        ]
+    matrix_c = \
+        [
+            [7, 1, -3]
+        ]
+
+    print(get_if_three_matrices_are_perpendicular(matrix_a=matrix_a,matrix_b=matrix_b,matrix_c=matrix_c,tab_amount=tab_amount))
---
+++ src/questions/7/7_1/q_7_1_3.py
@@ -0,0 +1,116 @@
+from traceback import print_list
+
+from src.A_universal_operations.calc_3_like_funcitons.get_if_three_matrices_are_perpandicular import \
+    get_if_three_matrices_are_perpendicular
+from src.A_universal_operations.display.print_matrix import print_matrix
+
+
+def q_7_1_3_a(tab_amount = "\t"):
+    tab_amount = "\t"
+
+    matrix_a = \
+        [
+            [1, 2, 3]
+        ]
+    matrix_b = \
+        [
+            [1, -1, 2]
+        ]
+    matrix_c = \
+        [
+            [7, 1, -3]
+        ]
+
+    return get_if_three_matrices_are_perpendicular(matrix_a=matrix_a, matrix_b=matrix_b, matrix_c=matrix_c,
+                                                  tab_amount=tab_amount)
+
+def q_7_1_3_b(tab_amount="\t"):
+    tab_amount = "\t"
+
+    matrix_a = \
+        [
+            [1, 2, 3]
+        ]
+    matrix_b = \
+        [
+            [1, -1, 2]
+        ]
+    matrix_c = \
+        [
+            [-7, -1, 3]
+        ]
+
+    return get_if_three_matrices_are_perpendicular(matrix_a=matrix_a, matrix_b=matrix_b, matrix_c=matrix_c,
+                                                  tab_amount=tab_amount)
+
+def q_7_1_3_c(tab_amount="\t"):
+    tab_amount = "\t"
+
+    matrix_a = \
+        [
+            [1, 2, 3]
+        ]
+    matrix_b = \
+        [
+            [1, -1, 2]
+        ]
+    matrix_c = \
+        [
+            [7, -1, -3]
+        ]
+
+    return get_if_three_matrices_are_perpendicular(matrix_a=matrix_a, matrix_b=matrix_b, matrix_c=matrix_c,
+                                                  tab_amount=tab_amount)
+
+def q_7_1_3_d(tab_amount="\t"):
+    tab_amount = "\t"
+
+    matrix_a = \
+        [
+            [1, 2, 3]
+        ]
+    matrix_b = \
+        [
+            [1, -1, 2]
+        ]
+    matrix_c = \
+        [
+            [1, -2, 6]
+        ]
+
+    return get_if_three_matrices_are_perpendicular(matrix_a=matrix_a, matrix_b=matrix_b, matrix_c=matrix_c,
+                                                  tab_amount=tab_amount)
+
+def q_7_1_3_e(tab_amount="\t"):
+    tab_amount = "\t"
+
+    matrix_a = \
+        [
+            [1, 2, 3]
+        ]
+    matrix_b = \
+        [
+            [1, -1, 2]
+        ]
+    matrix_c = \
+        [
+            [5, -5, 10]
+        ]
+
+    return get_if_three_matrices_are_perpendicular(matrix_a=matrix_a, matrix_b=matrix_b, matrix_c=matrix_c,
+                                                  tab_amount=tab_amount)
+
+if __name__ == "__main__":
+    """
+    answers_list = \
+        [
+            q_7_1_3_a(),
+            q_7_1_3_b(),
+            q_7_1_3_c(),
+            q_7_1_3_d(),
+            q_7_1_3_e()
+        ]
+
+    print_matrix(matrix_in_question=answers_list)
+    """
+    print(q_7_1_3_a())
