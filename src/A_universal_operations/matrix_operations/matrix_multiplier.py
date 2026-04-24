7003d25f21b80080f5d7377bdcd180099d234a76
Duncan Holmes <duncancholmes@gmail.com>
2026-04-23 20:04:19 -0500

    7.2.4 FREE ME

--- src/A_universal_operations/matrix_operations/matrix_multiplier.py
+++ src/A_universal_operations/matrix_operations/matrix_multiplier.py
@@ -81,3 +81,74 @@ if __name__ == "__main__":
     print_matrix(result_matrix)
     print("program ended")
 """
+
+def multiply_column_a_and_column_b(column_a, column_b, tab_amount="\t"):
+    """
+    these matrices ARE NOT nested. they're lists of numbers only.
+    so hlep me if you put in
+    [
+        [n]
+    ]
+    i'll find you and kiss you hehe :-3
+
+    :param column_a:
+    :param column_b:
+    :param tab_amount:
+    :return:
+    """
+    print(tab_amount, "multiply_columns_only")
+    tab_amount += "\t"
+
+    results_list = []
+
+    column_a_length = len(column_a)
+    column_b_length = len(column_b)
+
+    if(isinstance(column_a[0], list)):
+        exit("column_a is a list of lists. not cool bro")
+
+    if (isinstance(column_b[0], list)):
+        exit("column_a is a list of lists. not cool bro")
+
+    if(column_a_length != column_b_length):
+        print(tab_amount,column_a_length)
+        print(tab_amount,column_b_length)
+        exit("the length of column_a and column_b are not equal")
+
+    for i in range(column_a_length):
+        product = column_a[i] * column_b[i]
+        results_list.append(product)
+
+    return results_list
+
+def get_sum_of_column(column,tab_amount="\t"):
+    print(tab_amount, "get_sum_of_column")
+    tab_amount += "\t"
+
+    sum = 0
+    for i in range(len(column)):
+        sum += column[i]
+
+    return sum
+
+def multiply_column_and_scaler(column, scaler, tab_amount="\t"):
+    print(tab_amount, "multiply_column_and_scaler")
+    tab_amount += "\t"
+
+    result_column = []
+
+    for i in range(len(column)):
+        result_column.append(column[i] * scaler)
+
+    return result_column
+
+def add_column_a_and_column_b(column_a, column_b, tab_amount="\t"):
+    print(tab_amount, "add_column_a_and_column_b")
+    tab_amount += "\t"
+
+    added_column = []
+
+    for i in range(len(column_a)):
+        added_column.append(column_a[i])
+
+    return added_column
--- src/questions/7/7_2/q_7_2_4.py
+++ src/questions/7/7_2/q_7_2_4.py
@@ -1,5 +1,6 @@
 from src.A_universal_operations.matrix_operations import matrix_multiplier
-from src.A_universal_operations.matrix_operations.matrix_multiplier import multiply_matrix_universal
+from src.A_universal_operations.matrix_operations.matrix_multiplier import multiply_matrix_universal, \
+    multiply_column_a_and_column_b, get_sum_of_column, multiply_column_and_scaler, add_column_a_and_column_b
 from src.A_universal_operations.matrix_operations.matrix_operation_functions import *
 
 def q_7_2_4(tab_amount="\t"):
@@ -8,43 +9,73 @@ def q_7_2_4(tab_amount="\t"):
 
     v = \
     [
-        [10],
-        [6],
-        [-9],
-        [-8]
+        10,
+        6,
+        -9,
+        -8
     ]
     w1 = \
     [
-        [2],
-        [2],
-        [6],
-        [1]
+        2,
+        2,
+        6,
+        1
     ]
     w2 = \
     [
-        [0],
-        [2],
-        [3],
-        [-22]
+        0,
+        2,
+        3,
+        -22
     ]
 
     """
     (v - c1*u1 - c2*u2) * u1 = 0 
     (v - c1*u1 - c2*u2) * u2 = 0 
     """
-    product_v_w1 = multiply_matrix_universal(matrix_a=v,matrix_b=w1,tab_amount=tab_amount)
-    product_w1_w1 = multiply_matrix_universal(matrix_a=w1,matrix_b=w1,tab_amount=tab_amount)
-    product_w2_w1 = multiply_matrix_universal(matrix_a=w2,matrix_b=w1,tab_amount=tab_amount)
-    first_equation_result = [product_v_w1, product_w1_w1, product_w2_w1]
+    product_v_w1 = (multiply_column_a_and_column_b(column_a=v, column_b=w1, tab_amount=tab_amount))
+    product_w1_w1 = (multiply_column_a_and_column_b(column_a=w1, column_b=w1, tab_amount=tab_amount))
+    product_w2_w1 = (multiply_column_a_and_column_b(column_a=w2, column_b=w1, tab_amount=tab_amount))
+    sum_of_product_v_w1 = get_sum_of_column(column=product_v_w1,tab_amount=tab_amount)
+    sum_of_product_w1_w1 = get_sum_of_column(column=product_w1_w1,tab_amount=tab_amount)
+    sum_of_product_w2_w1 = get_sum_of_column(column=product_w2_w1,tab_amount=tab_amount)
 
     #(v - c1*u1 - c2*u2) * u2 = 0
-    product_v_w2 = multiply_matrix_universal(matrix_a=v, matrix_b=w2, tab_amount=tab_amount)
-    product_w1_w2 = multiply_matrix_universal(matrix_a=w1, matrix_b=w2, tab_amount=tab_amount)
-    product_w2_w2 = multiply_matrix_universal(matrix_a=w2, matrix_b=w2, tab_amount=tab_amount)
-    second_equation_results = [product_v_w2, product_w1_w2, product_w2_w2]
+    product_v_w2 = (multiply_column_a_and_column_b(column_a=v, column_b=w2, tab_amount=tab_amount))
+    product_w1_w2 = (multiply_column_a_and_column_b(column_a=w1, column_b=w2, tab_amount=tab_amount))
+    product_w2_w2 = (multiply_column_a_and_column_b(column_a=w2, column_b=w2, tab_amount=tab_amount))
+    sum_of_product_v_w2 = get_sum_of_column(column=product_v_w2,tab_amount=tab_amount)
+    sum_of_product_w1_w2 = get_sum_of_column(column=product_w1_w2,tab_amount=tab_amount)
+    sum_of_product_w2_w2 = get_sum_of_column(column=product_w2_w2,tab_amount=tab_amount)
 
-    print(first_equation_result)
-    print(second_equation_results)
+    print(tab_amount,sum_of_product_v_w1," + -",sum_of_product_w1_w1,"c1 + -",sum_of_product_w2_w1,"c2 = 0")
+    print(tab_amount,sum_of_product_v_w2," + -",sum_of_product_w1_w2,"c1 + -",sum_of_product_w2_w2,"c2 = 0")
+
+    """
+    from here you need to make the computer do algebra
+    i don't know how to do that bestie
+    so here it goes
+    
+    -30 + -45c1 + 0c2 = 0 
+    161 + -0c1 + -497c2 = 0 
+    -->
+    -45c1 = 30
+    -497c2 = -161
+    -->
+    c1 = -30/45 --> -2/3
+    c2 = -161/-497 --> 23/71 
+    -->
+    da_answer_lol = (w1 * -2/3) + (w2 * 23/71)
+    """
+    w1_scaled = multiply_column_and_scaler(column=w1, scaler=-2/3,tab_amount=tab_amount)
+    w2_scaled = multiply_column_and_scaler(column=w2, scaler=23/71,tab_amount=tab_amount)
+
+    print(w1_scaled)
+    print(w2_scaled)
+
+    sum_of_w1_scaled_and_w2_scaled = add_column_a_and_column_b(column_a=w1_scaled,column_b=w2_scaled,tab_amount=tab_amount)
+    print(sum_of_w1_scaled_and_w2_scaled)
+
     return "fuck"
 
 if __name__ == "__main__":
